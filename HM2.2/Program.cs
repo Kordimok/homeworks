f7968dc97be09f3f8c8f714847c8d29857cff560
Дмитрий <kordimok@yandex.ru>
2023-01-20 21:25:16 +0300

    HM2.2

---
+++ HM2.2/Program.cs
@@ -0,0 +1,12 @@
+﻿// Напишите программу, которая выводит случайное трехзначное число и удаляет вторую цифру этого числа.
+
+Console.Clear();
+
+int num = new Random ().Next (100,1000);
+
+int a1 = num / 100; 
+int a2 = num % 10;
+
+int result = a1*10 + a2;
+
+Console.WriteLine ($"{num}   --->    {result}");
