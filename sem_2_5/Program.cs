8d0756c354c35edeea642fddcf7ce54c457beb42
Дмитрий <kordimok@yandex.ru>
2023-01-20 22:42:40 +0300

    5 задача на 2 семинаре

---
+++ sem_2_5/Program.cs
@@ -0,0 +1,13 @@
+﻿// на вход подаетс два числа, опраделить является ли одно число квадратом другого
+
+Console.Write ("Введите первое число   ");
+int num1 = int.Parse(Console.ReadLine()! ) ;
+Console.Write ("Введите второе число    ");
+int num2 = int.Parse(Console.ReadLine()! ) ;
+
+if (num1*num1 == num2 || num2*num2 == num1 )
+{
+    Console.WriteLine ("da");   
+}
+else 
+ Console.WriteLine ("net");
