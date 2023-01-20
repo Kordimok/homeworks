7f78e9a03cbfb469b4a1f6f186ff1463e25d0a5a
Дмитрий <kordimok@yandex.ru>
2023-01-20 22:20:55 +0300

    3 задача на 2 семинаре

--- sem_2_3/Program.cs
+++ sem_2_3/Program.cs
@@ -1,23 +1,24 @@
-﻿// // Напишите программу, которая будет принимать на вход два числа и выводить, 
-// // является ли второе число кратным первому. 
-// // Если число 2 не кратно числу 1, то программа выводит остаток от деления
+﻿// Напишите программу, которая будет принимать на вход два числа и выводить, 
+// является ли второе число кратным первому. 
+// Если число 2 не кратно числу 1, то программа выводит остаток от деления
 
-// Console.Clear ();
+Console.Clear ();
 
-// Console.WriteLine ("Введите первое число :  ");
-// int a =  int.Parse(Console.ReadLine () );
+Console.WriteLine ("Введите первое число :  ");
+int a =  int.Parse(Console.ReadLine () );
 
-// Console.WriteLine ("Введите второе число :  ");
-// int b = int.Parse(Console.ReadLine () );
-// int ost = a % b; 
+Console.WriteLine ("Введите второе число :  ");
+int b = int.Parse(Console.ReadLine () );
+int ost = a % b; 
 
-// if (ost != 0)
-// {
-// Console.WriteLine ($" Не кратно, остаток равен {ost} ");
-// }
-// else 
-// {
-//   Console.WriteLine ($"Kратно");  
-// }
+if (ost != 0)
+{
+Console.WriteLine ($" Не кратно, остаток равен {ost} ");
+}
+else 
+{
+  Console.WriteLine ($"Kратно");  
+  
+}
 
 ______________________________
