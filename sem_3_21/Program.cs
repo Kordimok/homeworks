cce431848a60d4dc671ceb8a597efb9fb8507e3c
Дмитрий <kordimok@yandex.ru>
2023-01-23 22:32:50 +0300

    задача 21 на 3 семинаре

---
+++ sem_3_21/Program.cs
@@ -0,0 +1,21 @@
+﻿/*Задача №21. Работа в группах
+Напишите программу, которая принимает на вход
+координаты двух точек и находит расстояние между
+ними в 2D пространстве.
+● A (3,6); B (2,1) -> 5,09
+● A (7,-5); B (1,-1) -> 7,21*/
+
+Console.Clear();
+Console.Write("Введите координату х  =  ");
+double x1 = int.Parse(Console.ReadLine()!);
+Console.Write("Введите координату y  =  ");
+double y1 = int.Parse(Console.ReadLine()!);
+
+Console.Write("Введите координату х  =  ");
+double x2 = int.Parse(Console.ReadLine()!);
+Console.Write("Введите координату y  =  ");
+double y2 = int.Parse(Console.ReadLine()!);
+
+double result = Math.Sqrt((Math.Pow((x1 - x2), 2 ) + Math.Pow((y1 - y2), 2)));
+//result = Math.Round(result, 2);  //округление результата 
+Console.WriteLine($"Расстояние между заданными точками {result:f3}"); // f3 округление значения результата
