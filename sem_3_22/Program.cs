fd5e40abc229c791a9a7da2eeaa38a3e031f5143
Дмитрий <kordimok@yandex.ru>
2023-01-23 23:00:44 +0300

    задача 22 на 3 семинаре

---
+++ sem_3_22/Program.cs
@@ -0,0 +1,20 @@
+﻿/*Задача №22. Работа в группах
+Напишите программу, которая принимает на вход
+число (N) и выдаёт таблицу квадратов чисел
+от 1 до N.
+● 5 -> 1, 4, 9, 16, 25.
+● 2 -> 1,4  */
+
+Console.Clear();
+Console.Write("Введите любое число  ");
+double num = int.Parse(Console.ReadLine());    
+
+int count = 1;
+Console.Write($"{Math.Pow(count,2)}");
+count++;
+while (count <= num)
+{
+    double result = Math.Pow(count, 2);
+    Console.Write($", {result}" );
+    count++;
+}
