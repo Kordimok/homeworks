d9d71e02105bfd88c0b3f12a8ca9986058124b2a
Дмитрий <kordimok@yandex.ru>
2023-01-23 21:44:09 +0300

    задача 18 на 3 семинаре

---
+++ sem_3_18/Program.cs
@@ -0,0 +1,34 @@
+﻿/*Задача №18. Работа в группах
+Напишите программу, которая по заданному номеру
+четверти, показывает диапазон возможных координат
+точек в этой четверти (x и y).*/
+
+
+
+Console.Clear();
+Console.Write("Введите четверть =  ");
+int x = int.Parse(Console.ReadLine()!);
+
+
+if (x == 1 )
+{
+    Console.WriteLine ("Координаты Y > 0, X > 0");
+}
+ else if (x == 2)
+ {
+    Console.WriteLine ("Координаты Y > 0, X < 0");
+ }
+ else if (x == 3 )
+{
+    Console.WriteLine ("Координаты Y < 0, X < 0");
+    
+}
+else if (x == 4 )
+{
+    Console.WriteLine ("Координаты Y < 0, X > 0");
+    
+}
+if (  1 > x || x > 4  )
+{
+    Console.WriteLine ("Такой четверти не существует");
+}    
